8824c54be441663615d77c4da2b0249e159c00dc
Andrew Cannan <andrewmcannan@gmail.com>
2024-05-07 22:10:15 +0100

    add event listener to navbar and remove border

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './navbar.css'
 
 const NavBar = () => {
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            let clickover = event.target;
+            let opened = document.querySelector(".navbar-collapse").classList.contains("show");
+            if (opened && !clickover.classList.contains("navbar-toggler")) {
+                document.querySelector("button.navbar-toggler").click();
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid ms-3">
                 <a className="navbar-brand fs-1" href="#">ColorMuse</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler custom-navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarToggler">
