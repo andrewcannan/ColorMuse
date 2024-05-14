21f54356e62cb1b92f0bf8d4b6e6c4bac264e1c9
Andrew Cannan <andrewmcannan@gmail.com>
2024-05-14 22:14:15 +0100

    import google font and apply to brand in navbar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -21,7 +21,7 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid ms-3">
-                <a className="navbar-brand fs-1" href="/">ColorMuse</a>
+                <a className="navbar-brand my-brand fs-1" href="/">ColorMuse</a>
                 <button className="navbar-toggler custom-navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
