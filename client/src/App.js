d954a5bb0353cd9de3dcd5485d932837b1428d94
Andrew Cannan <andrewmcannan@gmail.com>
2024-05-14 20:55:36 +0100

    add router to app, create fileUpload component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import NavBar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
 import Home from './pages/home/home';
+import FileUpload from './components/fileUpload/fileUpload';
 
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@popperjs/core';
@@ -11,9 +13,14 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Home />
-      <Footer />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/generate' element={<FileUpload />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/fileUpload/fileUpload.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './fileUpload.css';
+
+const FileUpload = () => {
+    const [image, setImage] = useState(null);
+    const [disableInput, setDisableInput] = useState(false);
+
+    const handleFileChange = (e) => {
+        if (image) {
+            return;
+        }
+
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.onload = () => {
+            setImage(reader.result);
+            setDisableInput(true);
+        };
+        reader.readAsDataURL(file);
+    };
+
+    return (
+        <div className='temporary-container'>
+            <label className="custom-file-upload">
+                <input
+                    type="file"
+                    onChange={handleFileChange}
+                    accept="image/*"
+                    disabled={disableInput}
+                />
+                {image ? (
+                    <img src={image} alt="Uploaded" className="uploaded-image" />
+                ) : (
+                    <>
+                        <span><i class="fa-solid fa-plus"></i></span>
+                        <br />
+                        <span>Click to upload image.</span>
+                    </>
+                )}
+            </label>
+        </div>
+    );
+};
+
+export default FileUpload;
