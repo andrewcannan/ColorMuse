c86493e80bb51a30db871b1f963362704c61b801
Andrew Cannan <andrewmcannan@gmail.com>
2024-05-15 21:48:01 +0100

    refactored component structure to dynamically display colors

--- client/src/components/processImage/processImage.js
+++ client/src/components/processImage/processImage.js
@@ -1,35 +1,28 @@
-import React from "react";
-import './processImage.css';
+import React, { useState, useEffect } from "react";
+import "./processImage.css";
 
 const ProcessImage = () => {
+    const [ colors, setColors ] = useState(Array(5).fill(null))
+
     return (
         <div className="container-fluid process-container">
             <div className="row m-3" id="button-row">
                 <div className="col">
-                    <button class="btn btn-primary btn-lg btn-block" type="submit">Generate</button>
+                    <button className="btn btn-primary btn-lg btn-block" type="submit">Generate</button>
                 </div>
                 <div className="col">
-                    <button type="button" class="btn btn-outline-primary btn-lg btn-block">Save</button>
+                    <button type="button" className="btn btn-outline-primary btn-lg btn-block">Save</button>
                 </div>
             </div>
             <hr></hr>
             <div className="row m-3" id="color-row">
                 <div className="col">
-                    <div className="row border-gray px-0" id="color">
-                        <div className="col-3 gray"></div><div className="col-9" id="color-text">Colour</div>
+                    {colors.map((color) => (
+                        <div className="row border-gray px-0 color-display">
+                        <div className="col-3" style={{ backgroundColor: color || "lightgray" }}></div>
+                        <div className="col-9 color-text">Colour</div>
                     </div>
-                    <div className="row border-gray px-0" id="color">
-                        <div className="col-3 gray"></div><div className="col-9" id="color-text">Colour</div>
-                    </div>
-                    <div className="row border-gray px-0" id="color">
-                        <div className="col-3 gray"></div><div className="col-9" id="color-text">Colour</div>
-                    </div>
-                    <div className="row border-gray px-0" id="color">
-                        <div className="col-3 gray"></div><div className="col-9" id="color-text">Colour</div>
-                    </div>
-                    <div className="row border-gray px-0" id="color">
-                        <div className="col-3 gray"></div><div className="col-9" id="color-text">Colour</div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
