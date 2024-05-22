6ceaa4a05f9ce2c0345a23f48be796c97c25c89d
Andrew Cannan <andrewmcannan@gmail.com>
2024-05-22 21:05:10 +0100

    added endpoint to handle image processing and return hex values to client

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,28 @@
-from flask import Flask # type: ignore
+from flask import Flask, request
+from Pylette import extract_colors
 
 app = Flask(__name__)
 
 
-@app.route("/hello")
-def hello():
-    return("Hello from flask!")
+@app.route("/process_image", methods=['POST'])
+def process_image():
+    """
+    Extracts colors using Pylette library, calls rgb method on each returned value, converts to hex value and returns that to the client
+    """
+    image = request.files['image']
+    palette = extract_colors(image=image, palette_size=5, mode='MC', sort_mode='luminance')
+    
+    rgb_colors = [color.rgb for color in palette]
+    hex_colors = [rgb_to_hex(*rgb) for rgb in rgb_colors]
+
+    return {"colors": hex_colors}
+    
+
+def rgb_to_hex(r, g, b):
+    """
+    Converts rgb color to its hexidecimal value
+    """
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
 
 if __name__ == "__main__":
