24b9d23567d7f7c3abf4b4f99822a2f04bb5ce69
Andrew Cannan <andrewmcannan@gmail.com>
2024-05-22 19:59:59 +0100

    pass uploadedFile as prop and handle submit

--- client/src/components/processImage/processImage.js
+++ client/src/components/processImage/processImage.js
@@ -1,14 +1,35 @@
 import React, { useState, useEffect } from "react";
 import "./processImage.css";
 
-const ProcessImage = () => {
-    const [ colors, setColors ] = useState(Array(5).fill(null))
+const ProcessImage = ({ uploadedFile }) => {
+    const [colors, setColors] = useState(Array(5).fill(null))
+
+    const handleSubmit = async () => {
+        if (!uploadedFile) {
+            alert('Please select a file to be processed.')
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('image', uploadedFile)
+
+        try {
+            const response = await fetch('/process_image', {
+                method: 'POST',
+                body: formData,
+            });
+        }
+
+        catch {
+
+        }
+    }
 
     return (
         <div className="container-fluid process-container">
             <div className="row m-3" id="button-row">
                 <div className="col">
-                    <button className="btn btn-primary btn-lg btn-block" type="submit">Generate</button>
+                    <button type="button" onClick={handleSubmit} className="btn btn-primary btn-lg btn-block">Generate</button>
                 </div>
                 <div className="col">
                     <button type="button" className="btn btn-outline-primary btn-lg btn-block">Save</button>
@@ -18,9 +39,9 @@ const ProcessImage = () => {
                 <div className="col">
                     {colors.map((color) => (
                         <div className="row border-gray px-0 color-display">
-                        <div className="col-3" style={{ backgroundColor: color || "lightgray" }}></div>
-                        <div className="col-9 color-text">Colour</div>
-                    </div>
+                            <div className="col-3" style={{ backgroundColor: color || "lightgray" }}></div>
+                            <div className="col-9 color-text">Colour</div>
+                        </div>
                     ))}
                 </div>
             </div>
